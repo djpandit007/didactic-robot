4cf45ef145af604f4fd64a817840ea250b6a0f26
Dhananjay <dj.pandit007@gmail.com>
2021-03-28 19:34:33 -0700

    React forms and custom hooks

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function useForm(initial = {}) {
+  // Create a state object for our inputs
+  const [inputs, setInputs] = useState(initial);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    } else if (type === 'file') {
+      value[0] = e.target.files;
+    }
+
+    setInputs({
+      // Copy the existing state
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key]) => [key, ''])
+    );
+
+    setInputs(blankState);
+  }
+
+  // Return the things we want to surface from this custom hook
+  return { inputs, handleChange, resetForm, clearForm };
+}
