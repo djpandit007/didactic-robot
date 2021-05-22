40a0836d75f6fbfca9f5d66ec9aa285bfbd3fb1d
Dhananjay <dj.pandit007@gmail.com>
2021-05-22 08:37:20 -0700

    Updating items

---
+++ frontend/components/UpdateProduct.js
@@ -0,0 +1,114 @@
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_PRODUCT_MUTATION = gql`
+  mutation UPDATE_PRODUCT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateProduct(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // 1. We need to get existing product
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+
+  // 2. We need to get the mutation to update product
+  const [updateProduct, mutationResponse] = useMutation(
+    UPDATE_PRODUCT_MUTATION
+  );
+
+  //   2.5 Create state for form inputs
+  const { inputs, handleChange, clearForm } = useForm(data?.Product);
+
+  if (loading) return <p>Loading...</p>;
+  // 3. We need the form to handle the updates
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // Submit the input fields to backend
+        const res = await updateProduct({
+          variables: {
+            id,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
+          },
+        });
+
+        // const resp = await createProduct();
+        // clearForm();
+        // // Go to the product page
+        // Router.push({ pathname: `/product/${resp.data.createProduct.id}` });
+      }}
+    >
+      <DisplayError error={error || mutationResponse.error} />
+      <fieldset
+        disabled={mutationResponse.loading}
+        aria-busy={mutationResponse.loading}
+      >
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
+}
