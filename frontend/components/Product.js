df2d70159ddd4dc29c3f0111b1702366174be7ec
Dhananjay Pandit <djpandit007@users.noreply.github.com>
2021-05-22 09:13:08 -0700

    Module 6 - Working with Mutations (#7)

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Title from './styles/Title';
@@ -16,7 +17,12 @@ export default function Product({ product }) {
       </Title>
       <PriceTag>{formatMoney(product.price)}</PriceTag>
       <p>{product.description}</p>
-      {/* TODO: Add buttons to edit and delete item */}
+      <div className="buttonList">
+        <Link href={{ pathname: 'update', query: { id: product.id } }}>
+          Edit ✏️
+        </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
+      </div>
     </ItemStyles>
   );
 }
